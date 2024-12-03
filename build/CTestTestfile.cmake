ff8da56ff0e8602f670ce42540eab65584770a34
0profi <agarkov1206@gmail.com>
2024-12-03 20:19:49 +0300

    Add build directory to .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/daniil/Desktop/New_Daniil
-# Build directory: /home/daniil/Desktop/New_Daniil/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(BookTest "/home/daniil/Desktop/New_Daniil/build/BookTest")
-set_tests_properties(BookTest PROPERTIES  _BACKTRACE_TRIPLES "/home/daniil/Desktop/New_Daniil/CMakeLists.txt;19;add_test;/home/daniil/Desktop/New_Daniil/CMakeLists.txt;0;")
